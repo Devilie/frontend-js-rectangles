c93ea5f4716df165f1682b2cd3725cfb4b54565c
Devilie <devilie1989@gmail.com>
2016-12-12 01:54:08 +0200

    Add some raw code. Pass some tests.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-﻿
+function areIntersected(rect1, rect2)
+{
+    if (rect1.left>(rect2.top + rect2.width) ||
+        (rect1.top + rect1.width) < rect2.left ||
+        rect1.top > (rect2.left + rect2.height) ||
+        (rect1.left + rect1.height) > rect2.top)
+        {
+            return false;
+        }
+    else
+        {
+            return true;
+        }
+}
+
+function filterVisible(rect, rectArr)
+{
+    var visibleRectArray = rectArr.filter(areIntersected);
+    return visibleRectArray;
+}
