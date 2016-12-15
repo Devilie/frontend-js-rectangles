3d2b640ab29a7801af0ed25c05c7c260dd1add28
Devilie <devilie1989@gmail.com>
2016-12-15 15:55:55 +0200

    pass all tests

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
 function areIntersected(rect1, rect2)
 {
     return (
-        ((rect1.left > rect2.left && rect1.left < rect2.left+rect2.width)||
-         (rect1.left+rect1.width > rect2.left && rect1.left+rect1.width < rect2.left+rect2.width)       
-        )
-        &&
-        ((rect1.top > rect2.top && rect1.top < rect2.top+rect2.height)|| (rect1.top+rect1.height > rect2.top && rect1.top+rect1.height < rect2.top+rect2.height)
-        )
-        ||
-        (((rect2.left > rect1.left && rect2.left < rect1.left+rect1.width)||(rect2.left+rect2.width > rect1.left && rect2.left+rect2.width < rect1.left+rect1.width)
-         )
-         &&
-         ((rect2.top>rect1.top && rect2.top<rect1.top+rect1.height)||(rect2.top+rect2.height>rect1.top && rect2.top+rect2.height<rect1.top+rect1.height))
-        )
-        ||
-        (
-            (((rect1.left > rect2.left && rect1.left < rect2.left+rect2.width)||(rect1.left+rect1.width > rect2.left && rect1.left+rect1.width < rect2.left+rect2.width))
-             &&
-             ((rect2.top > rect1.top && rect2.top < rect1.top+rect1.height)||(rect2.top+rect2.height > rect1.top && rect2.top+rect2.height < rect1.top+rect1.height)))
+            (
+                (rect1.left > rect2.left && rect1.left < rect2.left+rect2.width)||
+                (rect1.left+rect1.width > rect2.left && rect1.left+rect1.width < rect2.left+rect2.width)       
+            )
+            &&
+            (
+                (rect1.top > rect2.top && rect1.top < rect2.top+rect2.height)|| 
+                (rect1.top+rect1.height > rect2.top && rect1.top+rect1.height < rect2.top+rect2.height)
+            )
             ||
-            (((rect2.left > rect1.left && rect2.left <rect2.left+rect1.width)||(rect2.left+rect2.width > rect1.left && rect2.left+rect2.width <rect1.left+rect1.width))
-             &&
-             ((rect1.top > rect2.top && rect1.top < rect2.top+rect2.height)||(rect1.top+rect1.height > rect2.top && rect1.top+rect1.height < rect2.top+rect2.height)))
-        )
-        );
+            (
+                (
+                    (rect2.left > rect1.left && rect2.left < rect1.left+rect1.width)||
+                    (rect2.left+rect2.width > rect1.left && rect2.left+rect2.width < rect1.left+rect1.width)
+                )
+                &&
+                (
+                    (rect2.top>rect1.top && rect2.top<rect1.top+rect1.height)||
+                    (rect2.top+rect2.height>rect1.top && rect2.top+rect2.height<rect1.top+rect1.height)
+                )
+            )
+            ||
+            (
+                (
+                    (
+                        (rect1.left > rect2.left && rect1.left < rect2.left+rect2.width)||
+                        (rect1.left+rect1.width > rect2.left && rect1.left+rect1.width < rect2.left+rect2.width)
+                    )
+                 &&
+                    (
+                        (rect2.top > rect1.top && rect2.top < rect1.top+rect1.height)||
+                        (rect2.top+rect2.height > rect1.top && rect2.top+rect2.height < rect1.top+rect1.height)
+                    )
+                )
+                ||
+                (
+                    (
+                        (rect2.left > rect1.left && rect2.left <rect2.left+rect1.width)||
+                        (rect2.left+rect2.width > rect1.left && rect2.left+rect2.width <rect1.left+rect1.width))
+                 &&
+                    (
+                        (rect1.top > rect2.top && rect1.top < rect2.top+rect2.height)||
+                        (rect1.top+rect1.height > rect2.top && rect1.top+rect1.height < rect2.top+rect2.height)))
+            )
+            ||
+            (
+                rect1.left == rect2.left && rect1.top == rect2.top && rect1.height == rect2.height && rect1.width == rect2.width
+            )
+        ); 
+        
 }
 
 
@@ -45,4 +72,4 @@ function filterVisible(rect, rectArr)
     return visibleRectArray;
 }
 
-console.log(areIntersected({left:0, top:0, width:20, height:20},{left:10, top:10, width:35, height:35}));
+console.log(areIntersected({left:0, top:0, width:20, height:20},{left:-10, top:-10, width:25, height:25}));
